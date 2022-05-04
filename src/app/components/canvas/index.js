e8bf1c6cfd188f6101418a7fff7e79edd57890fb
RyanWatts1 <devshop202@yahoo.com>
2022-05-04 09:21:43 -0400

    added canvas

---
+++ src/app/components/canvas/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+    background: #f6f6f6;
+    top:0;
+    left;
+    width;
+    height;
+    display;
+    place-items: center;
+`
